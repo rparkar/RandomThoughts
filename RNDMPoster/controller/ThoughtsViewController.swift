3abf719e9dcccff6a7e4d78d4f9b32d5a7231d51
Rehan Parkar <parkarcamp@gmail.com>
2018-05-21 13:43:49 -0500

    added thoughtsVC UI elements

---
+++ RNDMPoster/controller/ThoughtsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ThoughtsViewController.swift
+//  RNDMPoster
+//
+//  Created by Rehan Parkar on 2018-05-21.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
